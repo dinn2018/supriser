66567365ddc26fdcd22932574238fa4bcafe7cee
dinn <dinn.ding@vechain.com>
2020-03-09 01:54:13 +0800

    name changed

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-const HOSTURL = process.env.NODE_ENV == 'development' ? `http://localhost:3000` : 'http://supriser.com'
+const HOSTURL = process.env.NODE_ENV == 'development' ? `http://localhost:3000` : 'http://exanime.tv'
 export {
     HOSTURL,
 }
